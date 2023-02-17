a9908d6a5e0983f208a9399b8f79fd518abe3ff5
Jeremy Valenciano <jeremy.esau.valenci@gmail.com>
2023-02-16 18:32:36 -0600

    First portfolio version

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: '#fff',
+        bgColorBlack: "#040401",
+        purpleColor: "#6E07F3",
+        lightGray: "#CCD6F6",
+        tinyGray: "#8892B0"
+      }
+    },
+
+  },
+  plugins: [],
+}
